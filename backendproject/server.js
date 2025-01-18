e4709902c5079d77b61653df512b2fa9e77ac13b
Kaarlo Sasiang <earlsonnieandrew.plecerda1@gmail.com>
2025-01-18 15:33:02 +0800

    update CORS configuration to restrict origins to specific domains for enhanced security

--- backendproject/server.js
+++ backendproject/server.js
@@ -28,7 +28,7 @@ app.use(fileUpload());
 
 app.use(
   cors({
-    origin: "*",
+    origin: ["https://capstone2-1-two.vercel.app", "http://localhost:3000"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   })
